bc48c1f03c3649efcd22c3b05a859b0593ff3e77
ASUS-WCY <894295845@qq.com>
2021-10-21 16:00:07 +0800

    变量命名文件

---
+++ 01_react+react_redux+thunk/redux/constant.js
@@ -0,0 +1,13 @@
+// 命名文件 防止type名拼写错误
+
+const INCREMENT = 'increment'
+const DECREMENT = 'decrement'
+const INCREMENT_ASYNC = 'incrementAsync'
+const ADD_PERSON = 'addPerson'
+
+export {
+  INCREMENT,
+  DECREMENT,
+  INCREMENT_ASYNC,
+  ADD_PERSON,
+}
